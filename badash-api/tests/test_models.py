a3f87350a6d0ce73ab62acd9be92bb9429cacabc
swilcox <steven@wilcoxzone.com>
2017-11-07 08:46:28 -0600

    test delete cascade rules.

--- badash-api/tests/test_models.py
+++ badash-api/tests/test_models.py
@@ -9,6 +9,7 @@ class TestModelCreation(ApiTestCase):
     """Test Model Creation"""
 
     def test_dashboard(self):
+        """test creating a Dashboard model."""
         dash = Dashboard.objects.create(
             slug='testing',
             title='Testing Dashboard'
@@ -20,6 +21,7 @@ class TestModelCreation(ApiTestCase):
         self.assertEqual(dash2.slug, 'testing-again-the-dashboard')
 
     def test_job(self):
+        """test creating a Job model."""
         job = Job.objects.create(
             slug='test-job',
             title='Test Job'
@@ -27,6 +29,7 @@ class TestModelCreation(ApiTestCase):
         self.assertEqual(str(job), 'Test Job')
 
     def test_event(self):
+        """test creating an Event model."""
         job = Job.objects.create(slug='test-e-job', title='Test E Job')
         event = Event.objects.create(
             job=job,
@@ -39,6 +42,7 @@ class TestModelCreation(ApiTestCase):
         self.assertEqual(event.text_value, 'särskild')
 
     def test_display_field(self):
+        """test display_field property method"""
         job = Job.objects.create(
             slug='test-d-job',
             title='Test D Job',
@@ -55,8 +59,40 @@ class TestModelCreation(ApiTestCase):
         self.assertEqual(str(event), 'Test D Job {}: 0 (42.0)'.format(event.datetimestamp))
 
 
-class TestToDict(ApiTestCase):
+class TestDelete(ApiTestCase):
+    """Test Deletion Logic"""
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.job = Job.objects.create(
+            slug='test-job',
+            title='Test Job',
+            config={'display_field': 'value'}
+        )
+        cls.e1 = Event.objects.create(job=cls.job, result=0, value=40)
+        cls.e2 = Event.objects.create(job=cls.job, result=0, value=42, extra_text='särskild')
+        cls.e3 = Event.objects.create(job=cls.job, result=0, value=64)
+        cls.dashboard = Dashboard.objects.create(
+            slug='test-dashboard',
+            title='Test Dashboard'
+        )
+        cls.dashboard.jobs.append(cls.job)
+        cls.dashboard.save()
 
+    def test_delete(self):
+        """test delete cascade"""
+        # dashboard deletes should not cascade
+        self.assertEqual(1, Job.objects.all().count())
+        self.dashboard.delete()
+        self.assertEqual(1, Job.objects.all().count())
+        # job deletes *should* cascade
+        self.assertEqual(3, Event.objects.all().count())
+        self.job.delete()
+        self.assertEqual(0, Event.objects.all().count())
+
+
+class TestToDict(ApiTestCase):
+    """Test `to_dict` logic on Models"""
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -75,6 +111,7 @@ class TestToDict(ApiTestCase):
         cls.dashboard.jobs.append(cls.job)
 
     def test_job(self):
+        """test job.to_dict() method"""
         self.assertEqual(Event.objects.filter(job=self.job).count(), 3)
         self.assertEqual(json.loads(self.job.to_json())['title'], 'Test Job')
         job_dict = self.job.to_dict()
@@ -89,12 +126,14 @@ class TestToDict(ApiTestCase):
         self.assertEqual(job_dict['events'][0]['value'], self.e1.value)
 
     def test_dashboard(self):
+        """test dashboard.to_dict() method"""
         dash_dict = self.dashboard.to_dict()
         self.assertEqual(dash_dict['title'], 'Test Dashboard')
         self.assertEqual(len(dash_dict['jobs']), 1)
         self.assertEqual(dash_dict['jobs'][0], self.job.to_dict())
 
     def test_event(self):
+        """test event.to_dict() method"""
         event_dict = Event.objects.filter(job=self.job)[1].to_dict()
         self.assertEqual(len(event_dict['_id']), 24)
         self.assertEqual(len(event_dict['datetimestamp']), 26)
