5d8999f703bcb056b8fee4e6d3b6147effd2f367
swilcox <steven@wilcoxzone.com>
2018-02-03 22:28:10 -0600

    remove unused code

--- badash-api/utils.py
+++ badash-api/utils.py
@@ -5,9 +5,6 @@ from slugify import slugify as _slugify
 import bson
 
 
-# _slugify = Slugify(to_lower=True, stop_words=('the', 'a', 'an', 'is', 'of', 'in'))
-
-
 def slugify(input_string):
     """custom slugify"""
     return _slugify(input_string)
